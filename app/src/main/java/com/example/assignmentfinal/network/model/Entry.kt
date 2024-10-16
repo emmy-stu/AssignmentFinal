73dc4983ec108ff84b225c2b62be6cbf46f3ea7f
emmy-stu <chillyemre@hotmail.com>
2024-10-16 16:49:01 +1100

    Changed the values that is suitable based on 'languages'.

---
+++ app/src/main/java/com/example/assignmentfinal/network/model/Entry.kt
@@ -0,0 +1,11 @@
+package com.example.assignmentfinal.network.model
+
+data class Entry(
+    val name: String,
+    val family: String,
+    val branch: String,
+    val speakers: Int,
+    val writingSystem: String,
+    val officialIn: String,
+    val description: String
+)
