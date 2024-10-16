4a94507f718da9b97d49ea50929347a28d3d55dd
emmy-stu <chillyemre@hotmail.com>
2024-10-16 12:20:06 +1100

    Collecting all important code from lectures.

--- app/src/main/java/com/example/assignmentfinal/HomeScreen/MainActivity.kt
+++ app/src/main/java/com/example/assignmentfinal/HomeScreen/MainActivity.kt
@@ -1,14 +1,19 @@
-package com.example.assignmentfinal
+package com.example.assignmentfinal.HomeScreen
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("nit3213", "onCreate: ")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +21,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("nit3213", "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("nit3213", "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("nit3213", "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("nit3213", "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("nit3213", "onDestroy: ")
     }
 }
